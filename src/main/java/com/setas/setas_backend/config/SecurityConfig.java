24a014c385587523779b7fa2954e1e3579134532
Andyveloper <mcantillo1@icloud.com>
2026-05-14 22:06:45 -0500

    agregar dirección IP permitida en la configuración de CORS

--- src/main/java/com/setas/setas_backend/config/SecurityConfig.java
+++ src/main/java/com/setas/setas_backend/config/SecurityConfig.java
@@ -60,7 +60,7 @@ public class SecurityConfig {
   @Bean
   public CorsConfigurationSource corsConfigurationSource() {
     CorsConfiguration configuration = new CorsConfiguration();
-    configuration.setAllowedOrigins(List.of("http://localhost:5173", "http://localhost"));
+    configuration.setAllowedOrigins(List.of("http://localhost:5173", "http://localhost", "http://13.59.173.33"));
     configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH"));
     configuration.setAllowedHeaders(List.of("*"));
     configuration.setAllowCredentials(true);
